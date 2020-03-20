07b2a51055a07a61822537f44bd862b969eead2a
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-03-20 09:57:41 +0100

    shrink further every byte

--- test/index.js
+++ test/index.js
@@ -1,9 +1,15 @@
 const parse = require('../cjs');
 
-console.assert(parse(['<div />'], '!') === '<div></div>');
-console.assert(parse(['<div />'], '!', true) === '<div />');
-console.assert(parse(['<div test="', '" />'], '!') === '<div !0="test"></div>');
-console.assert(parse(['<div test=', ' />'], '!') === '<div !0="test"></div>');
-console.assert(parse(['<div>', '</div>'], '!') === '<div><!--!0--></div>');
-console.assert(parse(['<div> test="', '"</div>'], '!') === '<div> test="<!--!0-->"</div>');
-console.assert(parse(['test="', '"<br />'], '!') === 'test="<!--!0-->"<br />');
+const expect = (comp, result) => {
+  console.assert(comp === result, `expected "${result}" but got "${comp}"`);
+};
+
+expect(parse(['<div />'], '!'), '<div></div>');
+expect(parse(['<div />'], '!', true), '<div />');
+expect(parse(['<div test="', '" />'], '!'), '<div !0="test"></div>');
+expect(parse(['<div test=', ' />'], '!'), '<div !0="test"></div>');
+expect(parse(['<div a=', ' b=', ' />'], '!'), '<div !0="a" !1="b"></div>');
+expect(parse(['<div>', '</div>'], '!'), '<div><!--!0--></div>');
+expect(parse(['<div> test="', '"</div>'], '!'), '<div> test="<!--!0-->"</div>');
+expect(parse(['<div> a="', '" b="', '"</div>'], '!'), '<div> a="<!--!0-->" b="<!--!1-->"</div>');
+expect(parse(['test="', '"<br />'], '!'), 'test="<!--!0-->"<br />');
