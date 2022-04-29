fe15eff5e2a55cb310d8b69e572de8e0389cb1fa
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2022-04-29 09:48:35 +0200

    Fixed ambiguous hole parsing

--- test/index.js
+++ test/index.js
@@ -11,6 +11,7 @@ expect(parse(['<div\n\ttest="', '"\n/>'], '!'), '<div\n\t!0="test"></div>');
 expect(parse(['<div test=', ' />'], '!'), '<div !0=test></div>');
 expect(parse(['<div a=', ' b=', ' />'], '!'), '<div !0=a !1=b></div>');
 expect(parse(['<div>', '</div>'], '!'), '<div><!--!0--></div>');
+expect(parse(['<div>', '=</div>'], '!'), '<div><!--!0-->=</div>');
 expect(parse(['<div> test="', '"</div>'], '!'), '<div> test="<!--!0-->"</div>');
 expect(parse(['<div> a="', '" b="', '"</div>'], '!'), '<div> a="<!--!0-->" b="<!--!1-->"</div>');
 expect(parse(['test="', '"<br />'], '!'), 'test="<!--!0-->"<br />');
